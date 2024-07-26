1aa9df2adff5263d00e6696999bce9c6f430263e
Gabrielbprado <guardavege@gmail.com>
2024-07-26 11:27:43 -0300

    Adding Auto Mapper

---
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using MyRecipeBook.Communication.Requests.User;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Application.Services.AutoMapper;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<RequestRegisterUserJson,User>();
+    }
+}
