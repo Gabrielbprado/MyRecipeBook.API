9c8ffd22f8c7c6e175a5bda29f0028e3771ce795
Gabrielbprado <guardavege@gmail.com>
2024-08-12 18:03:03 -0300

    fix/Update Integration Tests

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,3 +1,4 @@
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ namespace WebApi.Test;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    public MyRecipeBook.Domain.Entities.User User { get; private set; } = default!;
+    public string Password { get; private set; } = string.Empty;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -28,7 +31,21 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     opts.UseInMemoryDatabase("InMemoryDbForTesting");
                     opts.UseInternalServiceProvider(provider);
                 });
+                
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<MyRecipeBookContext>();
+                db.Database.EnsureCreated();
+                InitializeDbForTests(db);
+                
             });
-
     }
+    
+    private void InitializeDbForTests(MyRecipeBookContext db)
+    {
+        (User,Password) = UserBuilder.Builder();
+        db.Users.Add(User);
+        db.SaveChanges();
+    }
+
 }
