6d43b0776e5b032c60602580ab19434e4d2f0b52
Gabrielbprado <guardavege@gmail.com>
2024-08-12 18:10:20 -0300

    feature/Adding Login Controller

---
+++ src/Backend/MyRecipeBook.API/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.Login;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses.User;
+
+namespace MyRecipeBook.API.Controllers;
+
+public class LoginController : MyRecipeBookControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login([FromServices] IDoLoginUseCase useCase, [FromBody] RequestLoginUseCase request)
+    {
+       var response = await useCase.Execute(request);
+        return Ok(response);
+    }
+}
