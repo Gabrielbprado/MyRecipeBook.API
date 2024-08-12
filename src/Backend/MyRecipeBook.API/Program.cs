8457f628367472fa4c577712110deb14972dc7f4
Gabrielbprado <guardavege@gmail.com>
2024-08-12 18:06:25 -0300

    feature/Adding Access Token and Update User Table

--- src/Backend/MyRecipeBook.API/Program.cs
+++ src/Backend/MyRecipeBook.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using MyRecipeBook.API.Filters;
 using MyRecipeBook.API.Middleware;
 using MyRecipeBook.Infrastructure.Data;
@@ -8,16 +9,49 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    
+    opts.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below. Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    
+    opts.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "Bearer",
+                Name = "Authorization",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+
+});
 builder.Services.AddControllers();
 builder.Services.AddMvc(opts =>
 {
     opts.Filters.Add(new ExceptionFilter());
 });
+builder.Services.AddRouting(opts =>
+{
+    opts.LowercaseUrls = true;
+});
 builder.Services.AddAllServices(builder.Configuration);
 builder.Services.AddFluentMigratorCore();
 var app = builder.Build();
-
 app.UseMiddleware<CultureMiddleware>();
 // Configure rotas para Controllers
 app.MapControllers();
