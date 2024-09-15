2476e75f51b52486630ac24b6a3b5485a2d6d685
Gabrielbprado <guardavege@gmail.com>
2024-09-15 12:14:17 -0300

    Feature/Adding Error Message for Recipe Validator and Adding Requests for the Recipe

---
+++ src/Shared/MyRecipeBook.Communication/Requests/Recipe/RequestIngredientJson.cs
@@ -0,0 +1,7 @@
+namespace MyRecipeBook.Communication.Requests.Recipe;
+
+public class RequestIngredientJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Quantity { get; set; } = string.Empty;
+}
